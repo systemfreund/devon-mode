6f9bfca3e44369e5472bc38f67eff34b3596fb7c
systemfreund <dev@nu.ll>
2024-08-09 11:32:33 +0200

    tests

--- run-tests.sh
+++ run-tests.sh
@@ -3,6 +3,6 @@ emacs -Q --batch \
       -L . \
       -l ert \
       -l devon.el \
+      -l tests/test-devon-stream.el \
       -l tests/devon-tests.el \
-      -l tests/checkpoint.el \
       -f ert-run-tests-batch-and-exit
